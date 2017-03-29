122e91553945ae6105677c71593063d8f6dd4222
spainhour <msspainhour@gmail.com>
2017-03-28 21:14:09 -0500

    added a course controller with ID tags
    Bare bones, no functionality yet

---
+++ src/edu/hendrix/csci250/csci250proj3/gui/EnhancedPlacementCourseGUIController.java
@@ -0,0 +1,34 @@
+package edu.hendrix.csci250.csci250proj3.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class EnhancedPlacementCourseGUIController {
+	
+	@FXML Label courseTitle;
+	@FXML Label courseCode;
+	@FXML Label instructor;
+	@FXML Label timeCode;
+	@FXML Label fastSearch;
+	@FXML Label location;
+	@FXML Label collegeCodes;
+	@FXML Label courseDescription;
+	@FXML Label preReq;
+	@FXML Button close;
+	
+	void initialize() {
+		
+	}
+	
+	@FXML
+	void close() {
+		try {
+			Stage stage = (Stage)close.getScene().getWindow();
+			stage.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
