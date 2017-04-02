920a17178d73dda9a111eabfa99ec52797008fe2
Jacob Turner <me@jacobturner.me>
2017-04-01 20:19:20 -0500

    fixed SQL code, it should completely work now

--- src/edu/hendrix/csci250/csci250proj3/SQL.java
+++ src/edu/hendrix/csci250/csci250proj3/SQL.java
@@ -12,22 +12,19 @@ public class SQL {
 	private Connection c;
 	private Statement stmt;
 	
-	public SQL() {
+	public Course getCourse(int fastSearchCode) {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:epdb.db");
-		} catch ( Exception e ) {
-			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 		}
-	}
-	
-	public Course getCourse(int fastSearchCode) {
 		Course courseData = null;
 		try {
 			stmt = c.createStatement();
 			ResultSet rs = stmt.executeQuery( "SELECT * FROM courses WHERE fast_search IN ('" + fastSearchCode + "') LIMIT 1;" );
 			while ( rs.next() ) {
-				ArrayList<String> instructors = new ArrayList<String>(Arrays.asList(rs.getString("instructors").split(";")));
+				ArrayList<String> instructors = new ArrayList<String>(Arrays.asList(rs.getString("instructors").split("; ")));
 				ArrayList<String> collegeCodes = new ArrayList<String>(Arrays.asList(rs.getString("college_codes").split(", ")));
 				courseData = new Course(rs.getString("course_code"), rs.getString("semester"), 
 								rs.getString("subject_code"), rs.getString("course_number"), 
@@ -40,16 +37,28 @@ public class SQL {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		try {
+			stmt.close();
+			c.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return courseData;
 	}
 	
 	public ArrayList<Course> getAllCourses() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:epdb.db");
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
 		ArrayList<Course> courseData = new ArrayList<Course>();
 		try {
 			stmt = c.createStatement();
 			ResultSet rs = stmt.executeQuery( "SELECT * FROM courses;" );
 			while ( rs.next() ) {
-				ArrayList<String> instructors = new ArrayList<String>(Arrays.asList(rs.getString("instructors").split(";")));
+				ArrayList<String> instructors = new ArrayList<String>(Arrays.asList(rs.getString("instructors").split("; ")));
 				ArrayList<String> collegeCodes = new ArrayList<String>(Arrays.asList(rs.getString("college_codes").split(", ")));
 				courseData.add(new Course(rs.getString("course_code"), rs.getString("semester"), 
 						rs.getString("subject_code"), rs.getString("course_number"), 
@@ -59,6 +68,12 @@ public class SQL {
 						collegeCodes));
 			}
 			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			stmt.close();
+			c.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
