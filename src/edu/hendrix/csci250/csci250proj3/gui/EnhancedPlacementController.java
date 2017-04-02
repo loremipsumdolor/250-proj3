5f387d8a313cd9be4321c7c43895e00c1f942e34
Jacob Turner <me@jacobturner.me>
2017-04-02 02:12:08 -0500

    added basic search

--- src/edu/hendrix/csci250/csci250proj3/gui/EnhancedPlacementController.java
+++ src/edu/hendrix/csci250/csci250proj3/gui/EnhancedPlacementController.java
@@ -1,10 +1,13 @@
 package edu.hendrix.csci250.csci250proj3.gui;
 
+import java.util.Optional;
+
 import edu.hendrix.csci250.csci250proj3.Course;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.Alert.AlertType;
 
 public class EnhancedPlacementController {
@@ -32,6 +35,18 @@ public class EnhancedPlacementController {
 		aboutBox.setHeaderText("About Enhanced Placement");
 		aboutBox.setContentText("Enhanced Placement v1.0\nCreated for Dr. Ferrer's CSCI 250 Spring 2017 class\n\nProject members:\n* Jacob Turner\n* Taylor Barker\n* Michael Spainhour\n* Uzair Tariq");
 		aboutBox.showAndWait();
+	}
+	
+	@FXML
+	private void basicSearch() {
+		TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle("Search - Enhanced Placement");
+		dialog.setHeaderText("Search Courses");
+		dialog.setContentText("Please enter a search term:");
+		Optional<String> result = dialog.showAndWait();
+		if (result.isPresent()){
+		    // SQL Code Here
+		}
 	}
 	
 	private void outputMessage(AlertType alertType, String message) {
